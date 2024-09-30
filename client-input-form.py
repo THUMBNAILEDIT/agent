e74a0425cac0e9258e506769d44e371ecc0a5a98
THUMBNAILED.IT <contact.thumbnailedit@gmail.com>
2024-09-30 18:25:06 +0300

    Flask Form

---
+++ client-input-form.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    video_description = request.form['description']
+    niche = request.form['niche']
+    return render_template('submit.html', video_description=video_description, niche=niche)
+
+if __name__ == '__main__':
+    app.run(debug=True)
