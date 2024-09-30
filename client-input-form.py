1c95b8dd7f284145d706b76135590fdcd2bb56ee
THUMBNAILED.IT <contact.thumbnailedit@gmail.com>
2024-09-30 19:20:14 +0300

    Supabase Connection

--- client-input-form.py
+++ client-input-form.py
@@ -1,3 +1,10 @@
+from supabase import create_client, Client
+
+url = "https://kblrsiucjbjucnyvftsz.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtibHJzaXVjamJqdWNueXZmdHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MTAzMjAsImV4cCI6MjA0MzI4NjMyMH0.bVfKoXML0OObYzfgx36m7w6qtJooO5zg7zSWTFx89kM"
+supabase: Client = create_client(url, key)
+
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,7 +17,14 @@ def index():
 def submit():
     video_description = request.form['description']
     niche = request.form['niche']
+
+    data = supabase.table('Video Data').insert({
+        "video_description": video_description,
+        "niche": niche
+    }).execute()
+
     return render_template('submit.html', video_description=video_description, niche=niche)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
